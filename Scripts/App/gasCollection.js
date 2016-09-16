0e93730fad86c89fafa9fcf80bd7dff14e8bb56b
Dmitry Fil <monkdaf@gmail.com>
2016-09-16 18:05:57 +0300

    One more changes and expiremens

--- Scripts/App/gasCollection.js
+++ Scripts/App/gasCollection.js
@@ -1,9 +1,38 @@
 ﻿// angular.module('indexApp')
 angular.module('indexAppDaf')
 
-app.controller('gasCollectionCtrl', ['$scope', 'indexService', '$state', 'roles', function ($scope, indexService, $state, roles) {
+  .config(['$stateProvider', function ($stateProvider) {
+    $stateProvider
+      // .state('gasCollection', {
+      //   url: '/gascollection',
+      //   templateUrl: 'Static/gascollection/index.html',
+      //   controller: 'gasCollectionCtrl',
+      //   // onEnter: function ($state, roles) {
+      //   //
+      //   //   if (!vmIsAuthorized('gasCollection', roles))
+      //   //     $state.go('app.error', { code: 'unauthorized' });
+      //   // }
+      // })
 
-    // controller's code here
+      .state({
+        name: 'gasCollection.reports',
+        url: '/reports',
+        //template: '<h3>It is area for reports</h3>'
+        templateUrl: 'Static/gascollection/reports.html'
+      })
+
+      .state({
+        name: 'gasCollection.trends',
+        url: '/trends',
+        // template: '<h3>It is area for trends</h3>'
+        templateUrl: 'Static/gascollection/trends.html'
+      });
+
+  }])
+
+.controller('gasCollectionCtrl', ['$scope', 'indexService', '$state', 'roles', function ($scope, indexService, $state, roles) {
+
+
     
 
 }]);
--- Scripts/App/indexDaf.js
+++ Scripts/App/indexDaf.js
@@ -5,16 +5,29 @@
 var app = angular.module('indexAppDaf', ['ui.router', 'pascalprecht.translate', 'ngSanitize'])
  .config(['$stateProvider', function ($stateProvider) {
    $stateProvider
-     .state({
-       name: 'reports',
-       url: '/reports',
-       template: '<h3>It is area for reports</h3>'
-     })
-
-     .state({
-     name: 'trends',
-     url: '/trends',
-     template: '<h3>It is area for trends</h3>'
+     .state('gasCollection', {
+       url: '/gascollection',
+       templateUrl: 'Static/gascollection/index.html',
+       //controller: 'gasCollectionCtrl',
+       // onEnter: function ($state, roles) {
+       //
+       //   if (!vmIsAuthorized('gasCollection', roles))
+       //     $state.go('app.error', { code: 'unauthorized' });
+       // }
      });
+
+     // .state({
+     //   name: 'reports',
+     //   url: '/reports',
+     //   //template: '<h3>It is area for reports</h3>'
+     //   templateUrl: 'Static/gascollection/reports.html'
+     // })
+     //
+     // .state({
+     // name: 'trends',
+     // url: '/trends',
+     // // template: '<h3>It is area for trends</h3>'
+     // templateUrl: 'Static/gascollection/trends.html'
+     // });
 
  }]);
