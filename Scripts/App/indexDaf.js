b1284d71de4005f3a5683c63c48f3b9a3f838747
Dmitry Fil <monkdaf@gmail.com>
2016-10-07 14:21:57 +0300

    Попытка применения n3 chart

--- Scripts/App/indexDaf.js
+++ Scripts/App/indexDaf.js
@@ -2,7 +2,7 @@
  * Created by daf on 16.09.2016.
  */
 
-var app = angular.module('indexAppDaf', ['ui.router', 'pascalprecht.translate', 'ngSanitize'])
+var app = angular.module('indexAppDaf', ['ui.router', 'pascalprecht.translate', 'ngSanitize', 'n3-line-chart'])
  .config(['$stateProvider', function ($stateProvider) {
    $stateProvider
      .state('gasCollection', {
